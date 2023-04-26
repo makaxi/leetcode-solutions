d7bc9dfd9f56ed2fffa3a14f154888f9ed01b1f2
makaxi <kamama919@gmail.com>
2023-04-26 12:52:01 -0700

    Solution to 217

---
+++ 217. Contains Duplicate.java
@@ -0,0 +1,21 @@
+/*
+ * 217. Contains Duplicate
+ * Given an integer array nums, return true if any value appears at least twice
+ * in the array, and return false if every element is distinct.
+ */
+
+ class Solution {
+  public boolean containsDuplicate(int[] nums) {
+      HashSet<Integer> map = new HashSet();
+      for(int num : nums){
+          if(map.contains(num)) return true;
+          map.add(num);
+      }
+      return false;
+  }
+}
+
+/*
+ * Time Complexity: O(n). Iterate through input array linearly 
+ * Space Complexity: O(n). Each index of nums is inputted into HashMap until we reach a duplicate
+ */
